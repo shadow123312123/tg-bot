42fd5771273ac0ab2f6a0265b223457e44fd5b74
Valera <valeraone@inbox.ru>
2023-05-13 14:47:43 +0300

    update

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ bot.on('message', async (msg) => {
         await bot.sendMessage(chatId, 'Ниже появится кнопка, заполни форму', {
             reply_markup: {
                 keyboard: [
-                    [{text: 'Заполнить форму', web_app: {url: webAppUrl + '/form'}}]
+                    [{text: 'Заполнить форму', web_app: {url: webAppUrl}}]
                 ]
             }
         })
